15cb4938f10c54d7891f8a918cd0d088ff4591b6
Babukuruvila <bobbyjohnamion@gmail.com>
2023-08-21 18:06:39 +0530

    changes

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '19680279'))
 API_HASH = environ.get('API_HASH', 'a32f974ade51b2dc74e8db4bb049ad01')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5904941859:AAEEKRJPrlm3df2OZTn0RauWM6vtJWgesz8')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5904941859:AAHDVSQv30p0uHHntf27l-VHdtwBizj9tZQ')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -48,10 +48,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Nathalia")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
-auth_channel = environ.get('AUTH_CHANNEL', '-1001978267456')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001830598481')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 # Set to False inside the bracket if you don't want to use Request Channel else set it to Channel ID
-REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1001978267456")
+REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1001830598481")
 REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
